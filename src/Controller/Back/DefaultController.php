1d28117960fcda4a724b33fbc340352d4280c9b5
TheoBourdel <bourdeltheo@gmail.com>
2023-03-29 19:31:15 +0200

    novice / pro role

--- src/Controller/Back/DefaultController.php
+++ src/Controller/Back/DefaultController.php
@@ -5,6 +5,9 @@ namespace App\Controller\Back;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends AbstractController
 {
@@ -14,9 +17,21 @@ class DefaultController extends AbstractController
         return $this->render('back/content/index.html.twig');
     }
 
-    #[Route('/choose-content', name: 'content_choose')]
-    public function chooseContent(): Response
+    #[Route('/{type}/choose-content', name: 'choose_content')]
+    public function becomeNovice(Request $request,UserRepository $userRepository): Response
     {
+        $user = $this->getUser();
+
+        if($request->get('type') == "novice") {
+            $user->setRoles(['ROLE_NOVICE']);
+        } else if($request->get('type') == "pro") {
+            $user->setRoles(['ROLE_PRO']);
+        } else {
+            throw $this->createNotFoundException('Le button est invalide');
+        }
+
+        $userRepository->save($user, true);
+
         return $this->render('back/content/choose.html.twig');
     }
 
