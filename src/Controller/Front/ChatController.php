860da65c24b28555049cdc449e8c2d410b342ac5
TheoBourdel <bourdeltheo@gmail.com>
2023-03-31 17:54:02 +0200

    chat fix

--- src/Controller/Front/ChatController.php
+++ src/Controller/Front/ChatController.php
@@ -13,12 +13,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\TagRepository;
 
 #[Route('/chat'), IsGranted('ROLE_USER')]
 class ChatController extends AbstractController
 {
     #[Route('/', name: 'app_chat')]
-    public function index(ConversationRepository $conversationRepository): Response
+    public function index(ConversationRepository $conversationRepository, TagRepository $tagRepository): Response
     {
         $conversation = $conversationRepository->findBy([
             'client' => $this->getUser(),
@@ -37,6 +38,7 @@ class ChatController extends AbstractController
         }
         return $this->render('front/chat/index.html.twig', [
             'conversation' => $conversation,
+            'tags' => $tagRepository->findAll()
         ]);
     }
 
